44b5991583299ed3fd337db39a12e357ee7f893f
Prakhar-1912 <rajs191201@gmail.com>
2023-07-14 21:08:50 +0530

    final commit

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,17 +1,35 @@
-import React,{useState} from "react";
+import React,{useState,useEffect} from "react";
 import { FiSettings } from "react-icons/fi";
 import { BiUserCircle } from "react-icons/bi";
 import { LuCalendarClock, LuPieChart } from "react-icons/lu";
 import { BsTags,BsFillArrowLeftCircleFill } from "react-icons/bs";
 
 const Sidebar = () => {
+    
     const[open,setOpen] = useState(true);
+
+    useEffect(() => {
+        const handleResize = () => {
+          if (window.innerWidth < 768) {
+            setOpen(false);
+          } else {
+            setOpen(true);
+          }
+        };
+    
+        window.addEventListener("resize", handleResize);
+        return () => {
+          window.removeEventListener("resize", handleResize);
+        };
+      }, []);
+    
   return (
     <div>
       <div className={`${open?"w-72":"w-20"} h-full bg-black text-white duration-300 m-5 p-5 rounded-3xl`}>
       <div className="relative">
-      <BsFillArrowLeftCircleFill className={` text-3xl rounded-full absolute -right-8 border cursor-pointer ${!open && "rotate-180"}`} onClick={()=>setOpen(!open)}/>
-      <p className="text-4xl pl-4 font-semibold">Board.</p>
+      <BsFillArrowLeftCircleFill className={` text-3xl rounded-full absolute -right-8 border border-black cursor-pointer ${!open && "rotate-180"}`} onClick={()=>setOpen(!open)}/>
+      <p className={`${!open&&"scale-0"} text-4xl pl-4 font-semibold`}>Board.</p>
+      
       </div>
         <div className="text-xl flex flex-col gap-60 pl-4">
           <ul>
